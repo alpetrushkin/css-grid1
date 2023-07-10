eae089db66f1e7fb2c448156af09d12d5a1d339b
alpetrushkin <alpetrushkin1984@mail.ru>
2023-07-10 20:17:57 +0300

    использование grid сетки

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+
+      <div className="list">
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="card__img" src="src/assets/images/lion.jpeg" alt="lion"/>
+          <div className="card__body">
+            <h3 className="card__title">Заголовок</h3>
+            <p className="card__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, eos!
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
